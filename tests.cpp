07e73795959135f1485f3d38d63674ec331a9f47
anitaprova <104943181+anitaprova@users.noreply.github.com>
2023-01-10 21:38:16 -0500

    added more tests

--- tests.cpp
+++ tests.cpp
@@ -3,16 +3,18 @@
 #include "funcs.h"
 #include <string>
 
-TEST_CASE("Methods and assigning") {
+TEST_CASE("Methods and Assigning") {
 	MyVector<int> i;
 	MyVector<std::string> s(5);
 	MyVector<double> d;
 	
 	d.push_back(6.5);
-	
+	i.push_back(5);
+	i.pop_back();
+
 	CHECK(i.empty() == 1);
 	CHECK(d.empty() == 0);
-	CHECK(d[0] ==  6.5);
+	CHECK(d[0] == 6.5);
 
 	CHECK(i.capacity() == 10);
 	CHECK(s.capacity() == 5);
@@ -20,4 +22,23 @@ TEST_CASE("Methods and assigning") {
 	CHECK(i.size() == 0);
 	CHECK(s.size() == 0);
 	CHECK(d.size() == 1);
+}
+
+TEST_CASE("Dynamic Array") {
+	MyVector<std::string> songs;
+	songs.push_back("Out of Touch");
+	songs.push_back("Lovers Rock");
+	songs.push_back("Hot Sugar");
+	songs.push_back("Washing Machine Heart");
+	songs.push_back("Little Dark Age");
+	songs.push_back("Tangerine");
+	songs.push_back("Agnes");
+	songs.push_back("7 Weeks & 3 Days");
+	songs.push_back("Meet Me");
+	songs.push_back("Die for You");
+	songs.push_back("Black Out Days"); //pass initial capacity 
+	
+	CHECK(songs.size() ==  11);
+	CHECK(songs.capacity() == 11);
+
 }
