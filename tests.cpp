1be60a6750753c89870bcd17fc3c517f03faadf4
anitaprova <104943181+anitaprova@users.noreply.github.com>
2023-01-10 21:22:22 -0500

    added more tests

--- tests.cpp
+++ tests.cpp
@@ -3,7 +3,7 @@
 #include "funcs.h"
 #include <string>
 
-TEST_CASE("All methods and assigning") {
+TEST_CASE("Methods and assigning") {
 	MyVector<int> i;
 	MyVector<std::string> s(5);
 	MyVector<double> d;
@@ -13,4 +13,11 @@ TEST_CASE("All methods and assigning") {
 	CHECK(i.empty() == 1);
 	CHECK(d.empty() == 0);
 	CHECK(d[0] ==  6.5);
+
+	CHECK(i.capacity() == 10);
+	CHECK(s.capacity() == 5);
+	CHECK(d.capacity() == 10);
+	CHECK(i.size() == 0);
+	CHECK(s.size() == 0);
+	CHECK(d.size() == 1);
 }
