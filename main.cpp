f18d6fff81fdf0bb19b96c0187d690c4450442cd
anitaprova <104943181+anitaprova@users.noreply.github.com>
2023-01-10 21:26:15 -0500

    finished

--- main.cpp
+++ main.cpp
@@ -4,14 +4,13 @@
 
 int main() {
 	MyVector<int> i; 
-	//std::cout << i.empty() << std::endl;
+	std::cout << "empty: " << i.empty() << std::endl;
 	std::cout << "size: " << i.size() << " capacity: " << i.capacity() << std::endl;
 	
 	i.push_back(5);	
 	i.push_back(8);
 	i.push_back(99);
 	i.push_back(1);
-	std::cout << "size: " << i.size() << " capacity: " << i.capacity() << std::endl;
 	i.push_back(5);
 	i.push_back(5);
 	i.push_back(7);
@@ -22,12 +21,15 @@ int main() {
 	i.print();
 	std::cout << "size: " << i.size() << " capacity: " << i.capacity() << std::endl;
 
-	/*i.pop_back();
-	std::cout << i.size() << std::endl;
-        std::cout << i.capacity() << std::endl;
+	i.pop_back();
 	i.print();
+	std::cout << "size: " << i.size() << " capacity: " << i.capacity() << std::endl;
+
+	i.clear();
+	i.print();
+        std::cout << "size: " << i.size() << " capacity: " << i.capacity() << std::endl;
 
 	std::cout << i[0] << std::endl;
-	*/
+	
 	return 0;
 }
